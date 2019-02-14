298afc9f6a1558c01a691f89720da09e44668f9d
NIC0001 <33195372+NIC0001@users.noreply.github.com>
2019-02-14 23:16:45 +0900

    Update document.sol

--- contracts/document.sol
+++ contracts/document.sol
@@ -1,4 +1,39 @@
 pragma solidity ^0.5.1;
 
 contract Document {
+  string title;
+  string [] author;
+  string [] sentence;
+  string [] journal;
+  address owner;
+  address [] publisher;
+ 
+  function getTitle() public{
+  }
+ 
+  function getAuthor() public{
+  }
+ 
+  function getSentence() public{
+  }
+  
+  function setPublisher(address addr) onlyOwner {
+  }
+ 
+  function setJournal(address addr) OnlyPublisher {
+  }
+ 
+  modifier onlyOwner {
+    require(msg.sender == owner);
+    _;
+  }
+  
+  modifier OnlyPublisher {
+    while(i < publisher.length){
+      require(msg.sender == publisher[i]);
+      i++;
+    }
+    _;
+  }
+  
 }
