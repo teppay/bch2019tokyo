684775309fb7492391d7c0f84fa9a2f0434180c7
NIC0001 <33195372+NIC0001@users.noreply.github.com>
2019-02-14 23:27:36 +0900

    Update publisher.sol

--- contracts/publisher.sol
+++ contracts/publisher.sol
@@ -1,4 +1,33 @@
 pragma solidity ^0.5.1;
 
 contract Publisher {
+  string Organization;
+  string url;
+  bytes certificates;
+  string [] journal;
+  mapping (address=> string) journal;
+  
+  function getOrganization() public returns(string) {
+  }
+ 
+  function getUrl() public returns(string) {
+  }
+ 
+  function getCertificates() public returns(bytes) {
+  }
+  
+  function setOwner(address addr) onlyOwner {
+  }
+ 
+  function setCertificates(bytes certificates) OnlyPublisher {
+  }
+  
+  function setJournal(address addr, string title) OnlyPublisher {
+  }
+ 
+  modifier onlyOwner {
+    require(msg.sender == owner);
+    _;
+  }
+  
 }
